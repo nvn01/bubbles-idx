936214b99d0b6871c93afef96a47cbf3be74e0aa
Novandra Anugrah <156439898+nvn01@users.noreply.github.com>
2026-02-05 23:36:08 +0700

    fix: show news section on mobile stock detail modal

--- src/components/StockDetailModal.tsx
+++ src/components/StockDetailModal.tsx
@@ -290,7 +290,7 @@ export function StockDetailModal({ stock, isOpen, onClose }: StockDetailModalPro
                 </div>
 
                 {/* RIGHT COLUMN */}
-                <div className="flex-1 flex flex-col min-w-0 bg-transparent overflow-hidden">
+                <div className="flex-1 flex flex-col min-w-0 bg-transparent">
                     {/* Chart header */}
                     <div
                         className="p-4 border-b flex items-center justify-between flex-wrap gap-2"
